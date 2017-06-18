d0094e9416b3b5e2060776183bede0033816a886
Borisstoy <boris.stoy@gmail.com>
2017-06-17 20:23:23 -0400

    Tweaks on navbar, flexbox cross browsers set up

--- app/helpers/project_requests_helper.rb
+++ app/helpers/project_requests_helper.rb
@@ -1,10 +1,8 @@
 module ProjectRequestsHelper
-
   def budget_range
     ranges = ["2,000€ - 3,000€",
              "3,000€ - 4,000€",
              " > 4000€"]
     ranges.each { |range| range }
   end
-
 end
