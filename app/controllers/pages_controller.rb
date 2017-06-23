1afaae9525047bf72e54cf06ece73f46c8e931d7
Borisstoy <boris.stoy@gmail.com>
2017-06-23 18:29:03 -0400

    Set up FAQ and fix layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :faq ]
 
   def home
+  end
+
+  def faq
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   scope '(:locale)', locale: /fr|en/ do
     devise_for :users
+    #pages
     root to: 'pages#home'
+    get 'qa' => 'pages#faq', as: :faq
+    #projects
     resources :project_requests
   end
 
