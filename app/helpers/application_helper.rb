a44c570091e271c1f9fd9ddadad6dae337eb8b53
Borisstoy <boris.stoy@gmail.com>
2017-07-23 00:13:46 -0400

    :wrench: Set up conditional layout for landings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def main_wrapper_page_style
+    wrap = ""
+    if is_path?("/travel")
+      wrap = "main-wrapper-travel"
+    else
+      wrap = ""
+    end
+    wrap
+  end
+
+  def is_path?(paths)
+    request.path.include?(paths)
+  end
 end
